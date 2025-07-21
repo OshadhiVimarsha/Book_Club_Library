cb009279ccffb65665134acff022c282d8e442f6
oshivimarsha <oshadivimarsha@gmail.com>
2025-07-21 11:46:26 +0530

    Add lending and Overdue books Manage

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import {Router} from "express";
 import authRouter from "./auth.routes";
 import bookRouter from "./book.routes";
 import readerRouter from "./reader.routes";
+import lendingRoutes from "./lending.routes";
 
 const rootRouter = Router()
 
 rootRouter.use("/auth", authRouter)
 rootRouter.use("/book", bookRouter)
 rootRouter.use("/reader", readerRouter)
+rootRouter.use("/lending", lendingRoutes)
 
 export default rootRouter;
