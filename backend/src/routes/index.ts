ce312d5c8e848abf1f3e1ae2c3af9687a5d27040
oshivimarsha <oshadivimarsha@gmail.com>
2025-07-21 10:00:33 +0530

    Add Reader Manage

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import {Router} from "express";
 import authRouter from "./auth.routes";
 import bookRouter from "./book.routes";
+import readerRouter from "./reader.routes";
 
 const rootRouter = Router()
 
 rootRouter.use("/auth", authRouter)
 rootRouter.use("/book", bookRouter)
+rootRouter.use("/reader", readerRouter)
 
 export default rootRouter;
